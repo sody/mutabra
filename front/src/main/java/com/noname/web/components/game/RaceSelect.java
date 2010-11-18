82fce1216ecb55c38322723cfb2af71ce2028f9c
Ivan Khalopik <ikhalopik@gmail.com>
2010-11-18 13:18:56 +0200

    races select now with pictures

--- front/src/main/java/com/noname/web/components/game/RaceSelect.java
+++ front/src/main/java/com/noname/web/components/game/RaceSelect.java
@@ -2,9 +2,11 @@ package com.noname.web.components.game;
 
 import com.noname.domain.common.Race;
 import com.noname.services.common.RaceService;
+import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.AssetSource;
 import org.greatage.tapestry.internal.SelectModelBuilder;
 
 import java.util.List;
@@ -30,10 +32,18 @@ public class RaceSelect {
 	@Property
 	private Object row;
 
+	@Inject
+	private AssetSource assetSource;
+
 	public List<Race> getRaces() {
 		if (races == null) {
 			races = raceService.getEntities();
 		}
 		return races;
+	}
+
+	public Asset getRaceImage() {
+		final String code = ((Race) row).getCode();
+		return assetSource.getContextAsset("img/races/" + code.toLowerCase() + ".png", null);
 	}
 }
