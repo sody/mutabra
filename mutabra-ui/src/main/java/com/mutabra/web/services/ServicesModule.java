1ecef5fce5b1f0e7ec460df7d635b074d7a7dafb
Ivan Khalopik <ikhalopik@gmail.com>
2011-03-20 20:34:14 +0200

    fixed hibernate repository configuration error

--- mutabra-ui/src/main/java/com/mutabra/web/services/ServicesModule.java
+++ mutabra-ui/src/main/java/com/mutabra/web/services/ServicesModule.java
@@ -16,11 +16,13 @@ import com.mutabra.services.security.*;
 import com.mutabra.web.services.i18n.Translator;
 import com.mutabra.web.services.i18n.TranslatorImpl;
 import org.greatage.domain.EntityFilterProcessor;
+import org.greatage.domain.EntityRepository;
 import org.greatage.domain.TransactionalAdvice;
 import org.greatage.domain.hibernate.HibernateAnnotationConfiguration;
 import org.greatage.domain.hibernate.HibernateExecutor;
 import org.greatage.domain.hibernate.HibernateModule;
 import org.greatage.ioc.Configuration;
+import org.greatage.ioc.MappedConfiguration;
 import org.greatage.ioc.ServiceBinder;
 import org.greatage.ioc.annotations.*;
 import org.greatage.ioc.proxy.Interceptor;
@@ -80,6 +82,24 @@ public class ServicesModule {
 		configuration.addInstance(CodedEntityFilterProcessor.class);
 		configuration.addInstance(TranslationFilterProcessor.class);
 		configuration.addInstance(AccountFilterProcessor.class);
+	}
+
+	@Contribute(EntityRepository.class)
+	public void contributeHibernateRepository(final MappedConfiguration<Class, Class> configuration) {
+		configuration.add(Translation.class, TranslatorImpl.class);
+		configuration.add(Account.class, AccountImpl.class);
+		configuration.add(Role.class, RoleImpl.class);
+		configuration.add(Permission.class, PermissionImpl.class);
+
+		configuration.add(Face.class, FaceImpl.class);
+		configuration.add(Race.class, RaceImpl.class);
+		configuration.add(Level.class, LevelImpl.class);
+		configuration.add(Card.class, CardImpl.class);
+		configuration.add(Effect.class, EffectImpl.class);
+		configuration.add(Summon.class, SummonImpl.class);
+
+		configuration.add(Hero.class, HeroImpl.class);
+		configuration.add(HeroCard.class, HeroCardImpl.class);
 	}
 
 	@Decorate(BaseEntityService.class)
