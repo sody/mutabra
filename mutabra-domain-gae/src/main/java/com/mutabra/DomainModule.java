a9e27e444e48039abb007525ba4c95b8c45a4c13
Ivan Khalopik <ikhalopik@gmail.com>
2011-06-19 01:19:40 +0300

    added appengine support

--- mutabra-domain-gae/src/main/java/com/mutabra/DomainModule.java
+++ mutabra-domain-gae/src/main/java/com/mutabra/DomainModule.java
@@ -1,44 +1,69 @@
 package com.mutabra;
 
+import com.mutabra.domain.Translation;
 import com.mutabra.domain.TranslationImpl;
-import com.mutabra.domain.common.CardImpl;
-import com.mutabra.domain.common.EffectImpl;
-import com.mutabra.domain.common.FaceImpl;
-import com.mutabra.domain.common.LevelImpl;
-import com.mutabra.domain.common.RaceImpl;
-import com.mutabra.domain.common.SummonImpl;
+import com.mutabra.domain.common.*;
+import com.mutabra.domain.player.Hero;
+import com.mutabra.domain.player.HeroCard;
 import com.mutabra.domain.player.HeroCardImpl;
 import com.mutabra.domain.player.HeroImpl;
-import com.mutabra.domain.security.AccountImpl;
-import com.mutabra.domain.security.PermissionImpl;
-import com.mutabra.domain.security.RoleImpl;
-import org.greatage.domain.hibernate.HibernateConfiguration;
+import com.mutabra.domain.security.*;
+import org.greatage.domain.BaseFilterProcessor;
+import org.greatage.domain.CompositeFilterProcessor;
+import org.greatage.domain.EntityFilterProcessor;
+import org.greatage.domain.EntityRepository;
+import org.greatage.domain.jdo.JdoExecutor;
+import org.greatage.domain.jdo.JdoExecutorImpl;
+import org.greatage.domain.jdo.JdoRepository;
 import org.greatage.inject.Configuration;
+import org.greatage.inject.MappedConfiguration;
+import org.greatage.inject.ServiceBinder;
+import org.greatage.inject.annotations.Bind;
+import org.greatage.inject.annotations.Build;
 import org.greatage.inject.annotations.Contribute;
-import org.greatage.inject.annotations.Named;
+import org.greatage.inject.annotations.Threaded;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
 
 /**
  * @author Ivan Khalopik
- * @since 8.0
+ * @since 1.0
  */
 public class DomainModule {
 
-	@Contribute(HibernateConfiguration.class)
-	@Named("HibernateAnnotationConfiguration")
-	public void contributeHibernateAnnotationConfiguration(final Configuration<Class> configuration) {
-		configuration.add(TranslationImpl.class);
-		configuration.add(AccountImpl.class);
-		configuration.add(RoleImpl.class);
-		configuration.add(PermissionImpl.class);
+	@Bind
+	public static void bind(final ServiceBinder binder) {
+		binder.bind(EntityFilterProcessor.class, CompositeFilterProcessor.class);
+		binder.bind(EntityRepository.class, JdoRepository.class);
+		binder.bind(JdoExecutor.class, JdoExecutorImpl.class).withScope(Threaded.class);
+	}
 
-		configuration.add(FaceImpl.class);
-		configuration.add(RaceImpl.class);
-		configuration.add(LevelImpl.class);
-		configuration.add(CardImpl.class);
-		configuration.add(EffectImpl.class);
-		configuration.add(SummonImpl.class);
+	@Build
+	public PersistenceManagerFactory buildPersistenceManagerFactory() {
+		return JDOHelper.getPersistenceManagerFactory("transactions-optional");
+	}
 
-		configuration.add(HeroImpl.class);
-		configuration.add(HeroCardImpl.class);
+	@Contribute(EntityFilterProcessor.class)
+	public void contributeEntityFilterProcessor(final Configuration<EntityFilterProcessor> configuration) {
+		configuration.addInstance(BaseFilterProcessor.class);
+	}
+
+	@Contribute(EntityRepository.class)
+	public void contributeEntityRepository(final MappedConfiguration<Class, Class> configuration) {
+		configuration.add(Translation.class, TranslationImpl.class);
+		configuration.add(Account.class, AccountImpl.class);
+		configuration.add(Role.class, RoleImpl.class);
+		configuration.add(Permission.class, PermissionImpl.class);
+
+		configuration.add(Face.class, FaceImpl.class);
+		configuration.add(Race.class, RaceImpl.class);
+		configuration.add(Level.class, LevelImpl.class);
+		configuration.add(Card.class, CardImpl.class);
+		configuration.add(Effect.class, EffectImpl.class);
+		configuration.add(Summon.class, SummonImpl.class);
+
+		configuration.add(Hero.class, HeroImpl.class);
+		configuration.add(HeroCard.class, HeroCardImpl.class);
 	}
 }
