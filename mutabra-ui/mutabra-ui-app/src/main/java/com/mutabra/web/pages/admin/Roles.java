57a7dfde24474eca707b849d8979329eb101302b
Ivan Khalopik <ikhalopik@gmail.com>
2011-09-03 17:17:50 +0300

    added tapestry grid styles

--- mutabra-ui/mutabra-ui-app/src/main/java/com/mutabra/web/pages/admin/Roles.java
+++ mutabra-ui/mutabra-ui-app/src/main/java/com/mutabra/web/pages/admin/Roles.java
@@ -8,6 +8,9 @@ import com.mutabra.web.internal.BaseEntityDataSource;
 import org.apache.tapestry5.grid.GridDataSource;
 import org.apache.tapestry5.ioc.annotations.InjectService;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 /**
  * @author Ivan Khalopik
  * @since 1.0
@@ -19,5 +22,12 @@ public class Roles extends AbstractPage {
 
 	public GridDataSource getRoleSource() {
 		return new BaseEntityDataSource<Role>(roleService.query(), Role.class);
+	}
+
+	void onAdd() {
+		final Role role = roleService.create();
+		final String randomCode = new BigInteger(130, new SecureRandom()).toString(32);
+		role.setCode(randomCode);
+		roleService.save(role);
 	}
 }
