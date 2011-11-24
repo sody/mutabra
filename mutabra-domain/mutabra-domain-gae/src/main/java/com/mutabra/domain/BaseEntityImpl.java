420f8a937f303b7edf01c41ba1708275ac605dbf
Ivan Khalopik <ikhalopik@gmail.com>
2011-11-24 19:58:27 +0300

    child entities equals method fixed

--- mutabra-domain/mutabra-domain-gae/src/main/java/com/mutabra/domain/BaseEntityImpl.java
+++ mutabra-domain/mutabra-domain-gae/src/main/java/com/mutabra/domain/BaseEntityImpl.java
@@ -2,6 +2,7 @@ package com.mutabra.domain;
 
 import com.googlecode.objectify.Key;
 import org.greatage.domain.AbstractEntity;
+import org.greatage.domain.Entity;
 
 import javax.persistence.Id;
 
@@ -25,5 +26,18 @@ public class BaseEntityImpl extends AbstractEntity<Long> implements BaseEntity {
 
 	public Key<?> getParentKey() {
 		return null;
+	}
+
+	@SuppressWarnings({"EqualsWhichDoesntCheckParameterClass"})
+	@Override
+	public boolean equals(final Object obj) {
+		final boolean result = super.equals(obj);
+		if (!result) {
+			return false;
+		}
+
+		final Key<?> parent = getParentKey();
+		final Key<?> objParent = ((BaseEntityImpl) obj).getParentKey();
+		return parent == null && objParent == null || parent != null && parent.equals(objParent);
 	}
 }
